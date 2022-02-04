dbbdb468a440045f7d1ab7a13d56596509cb924b
iSpoonz <ispoonz.codgamepedia@gmail.com>
2022-02-04 13:35:38 +0000

    added extra namespaces to search script

--- blank_edit.py
+++ blank_edit.py
@@ -1,15 +1,22 @@
 from log_into_wiki import *
 
-site = login('me', 'cod-esports')  # Set wiki
+site = login('bot', 'cod-esports')  # Set wiki
 
 limit = -1
 lmt = 0
+startat_page = 'Predictions:Call of Duty League/2020 Season/Launch Weekend/User/AaronJ76'
+passed_startat = False if startat_page else True
 
-pages = site.pages['Template:Infobox Tournament'].embeddedin()
+pages = site.pages['Template:UserPredictions'].embeddedin()
 
 for page in pages:
     if lmt == limit:
         break
+    if startat_page and page.name == startat_page:
+        passed_startat = True
+    if not passed_startat:
+        print("Skipping page %s" % page.name)
+        continue
     lmt += 1
     print('beginning page %s' % page.name)
     text = page.text()
--- search_wiki.py
+++ search_wiki.py
@@ -2,13 +2,23 @@ from mwrogue.esports_client import EsportsClient
 from mwrogue.auth_credentials import AuthCredentials
 
 credentials = AuthCredentials(user_file="me")
-site = EsportsClient('halo-esports', credentials=credentials)
+site = EsportsClient('cod-esports', credentials=credentials)
 
-site.search_namespace('player-name', 828)
+site.search_namespace('H3LL', 10006)
 
-# Namespace codes
+# -- Namespace codes --
+# https://cod-esports.fandom.com/wiki/Special:AllPages?from=&to=&namespace=0 (to find number)
 # Module: 828
 # MediaWiki: 8
+# Main: 0
+# Special: -1
+# Media: -2
+# File: 6
+# Template: 10
+# Help: 12
+# Category: 14
+# Predictions: 10010
+# Data: 10006
 
 
 
